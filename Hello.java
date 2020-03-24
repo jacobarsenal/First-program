c5bfd2d6f2836a3fde57de08d7cdf31481c710ae
Jacob Lam <JacobLam@harbour-capital.com>
2020-03-24 13:52:07 +0800

    Modified it a little bit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
+
+    String text = "Hello again";
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        new Hello().start();
+    }
+
+    private void start() {
+        System.out.println(text);
     }
 }
